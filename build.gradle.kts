46fe59777d0fda9941fb5865ec636ba8d2f87b66
mutz555 <mrbosscoc22@gmail.com>
2025-05-06 00:01:10 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,5 @@
-// Tidak perlu blok plugins di root, langsung buildscript saja jika hanya butuh classpath
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.highcapable.sweetdependency:sweetdependency:1.5.2")
-        classpath("com.highcapable.sweetproperty:sweetproperty:1.5.0")
-        // Tambahkan classpath lain jika perlu, misal Android Gradle Plugin, Kotlin Gradle Plugin, dsb.
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-    }
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
 }
