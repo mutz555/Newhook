8ec91adb1cc44a27a6bb9a8b81bbf952ae40916c
mutz555 <mrbosscoc22@gmail.com>
2025-05-06 01:37:08 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ dependencies {
     compileOnly(files("libs/api-82.jar")) 
 
     // Untuk YukiHookAPI dari JitPack
-    implementation("com.github.highcapable.YukiHookAPI:YukiHookAPI:1.1.7")
-ksp("com.github.highcapable.YukiHookAPI:ksp-xposed:1.1.7")
+    implementation("com.github.highcapable.YukiHookAPI:YukiHookAPI:1.2.1")
+ksp("com.github.highcapable.YukiHookAPI:ksp-xposed:1.2.1")
     implementation("com.github.duanhong169:drawabletoolbox:1.0.2") // Ganti versi sesuai kebutuhan
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
