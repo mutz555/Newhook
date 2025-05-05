3587ef6069a80ac826ac0ac88bec3c4a79236b20
mutz555 <mrbosscoc22@gmail.com>
2025-05-06 02:14:40 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
 }
- repositories {
-    google()
-    mavenCentral()
-    maven("https://jitpack.io")
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
 }
