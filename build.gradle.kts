caac4baee3ee75aaf77ee4fa75c4ad8a7b044531
mutz555 <mrbosscoc22@gmail.com>
2025-05-05 23:46:24 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,15 @@ plugins {
     autowire(libs.plugins.kotlin.android) apply false
     autowire(libs.plugins.kotlin.ksp) apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.highcapable.sweetdependency:sweetdependency:1.5.2")
+        classpath("com.highcapable.sweetproperty:sweetproperty:1.5.0")
+    }
+}
