42f900273fd88a24185f2e30ea9d0309392e29e8
mutz555 <mrbosscoc22@gmail.com>
2025-05-06 21:34:19 +0700

    Update HookEntry.kt

--- app/src/main/java/scope/hook/HookEntry.kt
+++ app/src/main/java/scope/hook/HookEntry.kt
@@ -1,29 +1,26 @@
 package scope.hook
 
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
-import com.highcapable.yukihookapi.hook.factory.*
-import com.highcapable.yukihookapi.hook.param.callback.result
+import com.highcapable.yukihookapi.hook.factory.hook
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.log.logger
+import com.highcapable.yukihookapi.hook.param.HookParam
 import com.highcapable.yukihookapi.hook.type.java.ClassType
-import com.highcapable.yukihookapi.YukiHookAPI
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
 @InjectYukiHookWithXposed
 class HookEntry : IYukiHookXposedInit {
 
-    override fun onInit() = configs {
-        // Konfigurasi YuKiHookAPI jika diperlukan
-    }
+    override fun onInit() = configs {}
 
     override fun onHook() = encase {
         ClassType.load("com.android.server.biometrics.sensors.fingerprint.FingerprintServiceStubImpl").hook {
             method {
                 name = "isFpHardwareDetected"
-            }.hookAll {
-                after {
-                    logger.debug("Method isFpHardwareDetected() terpanggil, return akan dipaksa true.")
-                    result = true
-                }
+            }.hookAll { hookParam: HookParam ->
+                hookParam.result = true
+                logger.debug("Method isFpHardwareDetected() terpanggil, return akan dipaksa true.")
             }
         }
     }
