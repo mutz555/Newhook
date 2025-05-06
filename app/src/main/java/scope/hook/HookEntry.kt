76da35e3184b58e044dcf4e3716aa061169eb7e5
mutz555 <mrbosscoc22@gmail.com>
2025-05-06 13:30:58 +0700

    Update HookEntry.kt

--- app/src/main/java/scope/hook/HookEntry.kt
+++ app/src/main/java/scope/hook/HookEntry.kt
@@ -5,6 +5,13 @@ import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 import com.highcapable.yukihookapi.YukiHookAPI
+import com.highcapable.yukihookapi.hook.type.java.loadClass
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.param.HookParam
+import com.highcapable.yukihookapi.hook.factory.hook
+import com.highcapable.yukihookapi.hook.factory.hookAll
+import com.highcapable.yukihookapi.hook.factory.after
+import com.highcapable.yukihookapi.hook.param.result
 
 @InjectYukiHookWithXposed
 class HookEntry : IYukiHookXposedInit {
